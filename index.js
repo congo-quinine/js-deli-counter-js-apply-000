95e6b07e2b0ed2ee8f29edde67774988ee862360
Rob Parker <congo.quinine@gmail.com>
2018-07-05 13:11:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function currentLine (line){
   let currentLine = `The line is currently: `;
   if(line.length > 0){
     for (let i=0; i<line.length; i++){
-        currentLine += `${i+1}. ${line[i]},`;
+        currentLine += `${i+1}. ${line[i]}, `;
         }
         return currentLine.slice(0 ,currentLine.length - 1);
           }else if (line.length === 0){
