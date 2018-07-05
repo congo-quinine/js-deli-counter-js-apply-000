2aeb4936f3c1e4c1eaa17c2dda9414ae6fbcec48
Rob Parker <congo.quinine@gmail.com>
2018-07-05 12:45:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //At the beginning of the day, the deli is empty and is represented by an empty array, e.g.,
-// 
+//
 // `var katzDeli = [];`
-// 
+//
 // 1. Build a function that a new customer will use when entering the deli. The function, `takeANumber`, should accept the current line of people, `katzDeliLine`, along with the new person's name as parameters. The function should return their position in line. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 // 
 
@@ -13,12 +13,12 @@ function takeANumber (line, name){
     line.push(name);
     //return a string and use array indexOF to find the position of the new customer and add 1 to give a non indexed number;
     return `Hello ${name}, your position in the line is ` + (line.indexOf(name)+1);
-  
+
 }
 takeANumber(katzDeliLine, "Ada");
 
 // 2. Build a function `nowServing`. This function should return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
-// 
+//
 
 function nowServing (line){
   //Check to see if line contains data
@@ -29,9 +29,9 @@ function nowServing (line){
             let serving = katzDeliLine.shift();
               return serving;
   }
-  
+
   return;
-  
+
 }
 nowServing(katzDeliLine);
 
@@ -40,9 +40,9 @@ nowServing(katzDeliLine);
 
 function currentLine (line){
   let currentLine = `The line is currently`;
-  if(line.length > 0){  
+  if(line.length > 0){
     for (let i=0; i<line.length; i++){
-        currentLine += ` ${i+1}, ${line[i]}`;    
+        currentLine += ` ${i+1}, ${line[i]}`;
         }
         return currentLine;
           }else if (line.length === 0){
@@ -50,4 +50,3 @@ function currentLine (line){
       }
 }
 currentLine(katzDeliLine);
-
