6a8b90baf8fd4a0a1f4cfe383a088a373a25ba7b
Rob Parker <congo.quinine@gmail.com>
2018-07-05 13:11:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ function currentLine (line){
   let currentLine = `The line is currently: `;
   if(line.length > 0){
     for (let i=0; i<line.length; i++){
-        currentLine += `${i+1}. ${line[i]}, `;
+        currentLine += `${i+1}. ${line[i]},`;
         }
-        return currentLine;
+        return currentLine.slice(0 ,currentLine.length - 1);
           }else if (line.length === 0){
             return 'The line is currently empty.';
       }
